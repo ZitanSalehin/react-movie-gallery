c1effa8c7c70d4c258e9f2f2219045c57478b372
Zitan Salehin <zitan-74@dipti.com.bd>
2022-02-28 20:07:13 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import Movie from './components/Movie';
+
+
+const FEATURED_API ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+
+const SEARCH_API ="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+function App() {
+  const [movies, setMovies]=useState([]);
+  const [searchTerm, setSearchTerm]= useState('');
+
+  useEffect(()=>{
+   
+   getMovies(FEATURED_API);
+  },[])
+
+const getMovies=(API)=>{
+  fetch(API)
+   .then((res)=>res.json())
+   .then((data)=>{
+     setMovies(data.results);
+    });
+}
+
+  const handleOnSubmit =(e)=>{
+    e.preventDefault();
+
+    if(searchTerm){
+    getMovies(SEARCH_API+searchTerm);
+
+    setSearchTerm('');
+  }
+};
+  const handleOnChange=(e)=>{
+    setSearchTerm(e.target.value);
+  }
+
+  return (
+    <div>
+      <div className='headerTitle'>
+    <h1>Movie Gallery App</h1>
+    </div>
+    <header>
+    
+      <form className='form' onSubmit={handleOnSubmit} onChange={handleOnChange}>
+        <input className='search' type='search' placeholder='Search...' value={searchTerm}/>
+      </form>
+
+      </header>
+
+    <div className="movie-container">
+      
+      {movies.length > 0 && movies.map((movie)=>(
+        <Movie key={movie.id} {...movie}/>
+      ))}
+    </div>
+    </div>
+  );
+}
+
+export default App;
